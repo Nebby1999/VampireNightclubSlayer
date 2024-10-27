49e1958fd49fba7107fefb885c306c918da735ef
Nebby <nebby131999@gmail.com>
2024-10-27 14:15:22 -0300

    mistakes where made, crunch time

---
+++ VampireNightclubSlayer/Assets/Scripts/VampireNightclubSlayerApplication.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VampireSlayer
+{
+    public class VampireNightclubSlayerApplication : Nebula.ApplicationBehaviour<VampireNightclubSlayerApplication>
+    {
+        protected override IEnumerator C_LoadGameContent()
+        {
+            yield break;
+        }
+    }
+}
